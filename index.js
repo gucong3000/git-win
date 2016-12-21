adad1f76e8a44f79a1c8d6887dbf1867dadca1a2
刘祺 <qil@jumei.com>
2016-12-21 16:46:05 +0800

    add shebang

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var assert = require('assert');
 var gitInstallPath = require('./lib/git-path')();
 assert.ok(gitInstallPath, 'Git not found, please install Git and try again.\nhttps://git-for-windows.github.io/\nhttps://npm.taobao.org/mirrors/git-for-windows/');
