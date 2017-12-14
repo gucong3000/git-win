fb4c4c6e7fa5701fd1a3c29639827822be6d8c4a
刘祺 <gucong@gmail.com>
2017-12-14 17:52:21 +0800

    Split test Cases

---
+++ test/api.js
@@ -0,0 +1,36 @@
+'use strict';
+const expect = require('chai').expect;
+const fs = require('fs-extra');
+
+describe('git path', () => {
+	const git = require('../src/');
+	const gitPath = require('../src/git-path.js');
+
+	it('get git dir', () => {
+		expect(git).to.be.a('string');
+	});
+
+	it('git dir exist', async () => {
+		await expect(
+			fs.stat(git).then(stats => stats.isDirectory())
+		).eventually.to.be.true;
+	});
+
+	it('get git dir by `PATH` env', () => {
+		expect(
+			gitPath.getGitDirByPathEnv()
+		).to.equal(git);
+	});
+
+	it('get git dir form registry', () => {
+		expect(
+			gitPath.getGitDirByRegstry() || gitPath.getGitDirByRegstry(64) || gitPath.getGitDirByRegstry(32)
+		).to.equal(git);
+	});
+
+	it('lookup for git dir', () => {
+		expect(
+			gitPath.lookupGitDir()
+		).to.equal(git);
+	});
+});
---
+++ test/release.js
@@ -0,0 +1,16 @@
+'use strict';
+const expect = require('chai').expect;
+const proxyquire = require('proxyquire');
+
+describe('git release', () => {
+	it('use cache when network failure', async () => {
+		const getRelease = proxyquire('../src/get-release', {
+			got: () => (
+				Promise.reject(new Error('Network failure'))
+			),
+		});
+		await expect(
+			getRelease(2)
+		).eventually.to.have.property('id', 8710511);
+	});
+});
